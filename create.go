56d66120cef4f5a8695600b228a0df8bdc6e70d9
Makenzie T <77457611+MTheurer@users.noreply.github.com>
2021-02-02 15:15:45 -0700

    Add files via upload

---
+++ create.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+//RandomString generates a random string of A-Z chars
+func RandomString(len int) string {
+	bytes := make([]byte, len)
+
+	for i := 0; i < len; i++ {
+		//A=65 and Z = 65+25
+		bytes[i] = byte(65 + rand.Intn(25))
+	}
+	return string(bytes)
+}
+
+//createKey decodes the recieved Json and detects errors
+func createKey(w http.ResponseWriter, r *http.Request) {
+	mutex := &sync.Mutex{}
+	log.Println("starting creatKey server")
+	w.Header().Set("Content-Type", "application/json")
+
+	var newUrl urlFormat
+	_, error := url.ParseRequestURI(newUrl.Url)
+	if error != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, `{"Error": "406: Not an Acceptable URL"}`)
+		return
+	}
+
+	fmt.Println("decoding newKey")
+	err := json.NewDecoder(r.Body).Decode(&newUrl)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println(`{"Error": "400: "Failed to encode json}"`)
+		return
+	}
+
+	//implimented mutexes for no creation overlaps
+	mutex.Lock()
+
+	newString := RandomString(8)
+	urlMap[newString] = newUrl.Url
+
+	mutex.Unlock()
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, `{"short_url": "http://127.0.0.1:5555/%s"}`, newString)
+	log.Println(urlMap)
+	return
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type urlFormat struct {
+	Url string `json:"url"`
+}
+
+var urlMap = make(map[string]string)
+
+// HomePage func to return Homepage text
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my ShortyResty :)")
+}
+
+func main() {
+
+	urlMap["ASDFASDF"] = "http://example.com/verylonguselessURLthatdoesnotseemtoend/123"
+
+	HandleRequests()
+}
+
+func HandleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", HomePage).Methods("GET")
+	myRouter.HandleFunc("/shorten/{id}", urlShortener).Methods("GET")
+	myRouter.HandleFunc("/create", createKey).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
